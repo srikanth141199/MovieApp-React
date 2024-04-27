4183f0b880de06c51bed82426b76003e80527d07
Srikanth K <kolleparasrikanth@gmail.com>
2024-04-27 12:19:24 +0530

    Card JSX Implemented

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -3,7 +3,25 @@ import React from "react";
 class MovieCard extends React.Component{
 
     render(){
-        return <h2>This is Movie Card</h2>
+        return <div className="main">
+            <div className="movie-card">
+                <div className="left">
+                    <img src="" alt="Poster" />
+                </div>
+                <div className="right">
+                    <div className="title"></div>
+                    <div className="plot"></div>
+                    <div className="price"></div>
+
+                    <div className="footer">
+                        <div className="rating"></div>
+                        <div className="stars"></div>
+                        <button className="favourite-btn">Favourites</button>
+                        <button className="cart-btn">Cart BTN</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     }
 }
 
