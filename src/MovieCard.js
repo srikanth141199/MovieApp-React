9670e4db5385073de4b1babbfe467d1cac754378
Srikanth K <kolleparasrikanth@gmail.com>
2024-04-27 12:38:28 +0530

    Card design changes

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -9,14 +9,19 @@ class MovieCard extends React.Component{
                     <img src="https://rukminim2.flixcart.com/image/850/1000/jv2p6kw0/poster/y/n/h/medium-endgame3jumbo-avengers-endgame-jumbo-poster-for-room-original-imafgyfvxjzuvkfu.jpeg?q=20&crop=false" alt="Poster" />
                 </div>
                 <div className="right">
-                    <div className="title"></div>
-                    <div className="plot"></div>
-                    <div className="price"></div>
+                    <div className="title">The Avengers</div>
+                    <div className="plot">Super Natural Fictional World</div>
+                    <div className="price">Rs: 299</div>
 
                     <div className="footer">
-                        <div className="rating"></div>
-                        <div className="stars"></div>
-                        <button className="favourite-btn">Favourites</button>
+                        <div className="rating">8.9</div>
+                        <div className="star-dis">
+                            <img src = "https://cdn-icons-png.flaticon.com/128/992/992683.png" alt="minus" className="str-btn"/>
+                            <img src = "https://cdn-icons-png.flaticon.com/128/477/477406.png" alt="star" className="stars"/>
+                            <img src="https://cdn-icons-png.flaticon.com/128/992/992651.png" alt="plus" className="str-btn"/>
+                            <span>0</span>
+                        </div>
+                        <button className="favourite-btn">Favorites</button>
                         <button className="cart-btn">Cart BTN</button>
                     </div>
                 </div>
