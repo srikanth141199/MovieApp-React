f96b8d75d3f1c066e306f6c5150c9af953f2e018
Srikanth K <kolleparasrikanth@gmail.com>
2024-04-27 12:12:05 +0530

    first Commit, Movie App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MovieCard from "./MovieCard";
+
+function App() {
+  return (
+    <>
+    <h1>Movie App</h1>
+    <MovieCard/>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/MovieCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class MovieCard extends React.Component{
+
+    render(){
+        return <h2>This is Movie Card</h2>
+    }
+}
+
+export default MovieCard;
