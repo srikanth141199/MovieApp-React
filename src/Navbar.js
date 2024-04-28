dfdc09143a31e8b372245e1f39d2e8736dd27bbe
Srikanth K <kolleparasrikanth@gmail.com>
2024-04-28 08:03:34 +0530

    Navbar initiation

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Navbar extends React.Component{
+
+    render(){
+        return <>
+            <div>
+                <div>Title</div>
+                <div>
+                    <img src="" alt="Cart Icon" />
+                </div>
+            </div>
+        </>
+    }
+} 
